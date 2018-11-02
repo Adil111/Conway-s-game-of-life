260388fe4fcb823ad159305bfa35890a142fbbd6
Adil Sharipov <Sharipov.2013@yandex.ru>
2018-11-02 23:12:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,14 +6,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main {
-    static PixelComponentBlack black;
-    static PixelComponentWhite white;
-    static Graphics graphics;
-    static GameFrame frame;
-    static InputFrame frame2;
+    private static PixelComponent blackPixel;
+    private static PixelComponent whitePixel;
+    private static Graphics graphics;
+    private static GameFrame gameFrame;
+    private static InputFrame inputFrame;
     public static void main(String[] args) {
-        //TODO: rewrite gameThread and Main (add getters)
-        //TODO: rewrite Pixels(make one class instead two)!!
+        //TODO: rewrite gameThread and Main (add getters) — Done
+        //TODO: rewrite Pixels(make one class instead two)!! — Done
         //TODO: add statistics on close
         //*TODO: add checking by last 10 turns after every 100 turns, reduce every turn checking
         //TODO: check values on start ~ rewrite InputFrame
@@ -26,15 +26,15 @@ public class Main {
     }
 
     private static void waitForStart() {
-        frame2 = new InputFrame();
-        frame2.start.addActionListener(new ActionListener() {
+        inputFrame = new InputFrame();
+        inputFrame.start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Constants.FILL_PERCENT = Integer.parseInt(frame2.getFillPercent().getText());
-                Constants.DEATH_CHANCE = Integer.parseInt(frame2.getDeathChance().getText());
-                Constants.LIFE_CHANCE = Integer.parseInt(frame2.getLifeChance().getText());
-                Constants.DELAY = Integer.parseInt(frame2.getDelayMS().getText());
-                JOptionPane.showMessageDialog(frame2, "Игра началась!");
+                Constants.FILL_PERCENT = Integer.parseInt(inputFrame.getFillPercent().getText());
+                Constants.DEATH_CHANCE = Integer.parseInt(inputFrame.getDeathChance().getText());
+                Constants.LIFE_CHANCE = Integer.parseInt(inputFrame.getLifeChance().getText());
+                Constants.DELAY = Integer.parseInt(inputFrame.getDelayMS().getText());
+                JOptionPane.showMessageDialog(inputFrame, "Игра началась!");
                 GameThread thread = new GameThread();
                 thread.start();
             }
@@ -43,13 +43,29 @@ public class Main {
 
     public static void initGUI() {
         Field.create();
-        frame = new GameFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
+        gameFrame = new GameFrame();
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setVisible(true);
+        gameFrame.setLocationRelativeTo(null);
 
-        graphics = frame.getGraphics();
-        black = new PixelComponentBlack();
-        white = new PixelComponentWhite();
+        graphics = gameFrame.getGraphics();
+        blackPixel = new PixelComponent(Color.BLACK);
+        whitePixel = new PixelComponent(Color.WHITE);
+    }
+
+    public static PixelComponent getBlackPixel() {
+        return blackPixel;
+    }
+
+    public static PixelComponent getWhitePixel() {
+        return whitePixel;
+    }
+
+    public static InputFrame getInputFrame(){
+        return inputFrame;
+    }
+
+    public static Graphics getGraphics(){
+        return graphics;
     }
 }
